c126deed45ef4f1c16cc366c6a6c34655f3da160
floatingmess06 <mramanrd1465@gmail.com>
2022-09-07 17:41:07 +0530

    Contribution by floatingmess06

---
+++ scripts/hello_world_floaingmess06.js
@@ -0,0 +1,37 @@
+// LANGUAGE: Javascript
+// ENV: Node.js
+// AUTHOR: Aman Dashore
+// GITHUB: https://github.com/floatingmess06
+
+console.log('Hello World');
+// c : 
+//
+//#include<stdio.h>
+//void main(){
+// printf("Hello World");
+// }
+//
+// cpp : 
+//
+// #include<iostream>
+// using namespace std;
+// int main(){
+// cout<<"Hello World";
+// return 0;
+// }
+//
+// python :
+//
+// print("Hello World")
+//
+// java :
+//
+// package com.Quicklessonsjava;
+//
+// public class helloWorld{
+// 	public static void main(String[] args){
+// 	System.out.println("Hello World");
+// 	}
+// }
+//
+// ----- end -----
